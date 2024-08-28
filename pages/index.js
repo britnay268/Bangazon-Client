c8f7ac1142a4832ecbbaadb22b9923d9afa02700
Britnay Gore <britnaygore268@gmail.com>
2024-08-27 20:46:19 -0400

    There is a problem with continuous fetching that led to my backend now not starting

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,74 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
+import { useEffect, useState } from 'react';
+import { Button, Table } from 'react-bootstrap';
+import Link from 'next/link';
+import { getLatestProducts } from '../api/ProductData';
+// import { signOut } from '../utils/auth';
+// import { useAuth } from '../utils/context/authContext';
 
 function Home() {
-  const { user } = useAuth();
+  // const { user } = useAuth();
+  const [products, setProducts] = useState([]);
+
+  // const theLatestProducts = () => {
+  //   getLatestProducts().then(setProducts);
+  // };
+
+  getLatestProducts().then(setProducts);
+  // console.warn(products);
+
+  useEffect(() => {
+    getLatestProducts().then(setProducts);
+    // theLatestProducts();
+  }, [products]);
 
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.fbUser.displayName}! </h1>
+    <div>
+      {/* <h1>Hello {user.fbUser.displayName}! </h1>
       <p>Your Bio: {user.bio}</p>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out
-      </Button>
+      </Button> */}
+      <Table>
+        <thead>
+          <tr>
+            <th colSpan={8}>#</th>
+            {/* <th colSpan={8}>Last Name</th>
+            <th>Username</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            products.map((p) => (
+              <tr>
+                <td>{p.name}</td>
+                {/* <td colSpan={8}>Mark</td>
+                <td>@mdo</td> */}
+                <td>
+                  <Link passHref href={`/products/${p.Id}`}>
+                    <Button variant="link">Link</Button>
+                  </Link>
+                </td>
+              </tr>
+            ))
+          }
+          {/* <tr>
+            <td>1</td>
+            <td colSpan={8}>Mark</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td colSpan={8}>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={8}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr> */}
+        </tbody>
+      </Table>
     </div>
   );
 }
