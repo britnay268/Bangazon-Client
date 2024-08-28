00675184f1e41cacfbf83f994de5749e176a4fd1
Britnay Gore <britnaygore268@gmail.com>
2024-08-27 23:06:00 -0400

    Resolved issue and show products with links to details page and button to see all products

--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,25 @@ function Home() {
   //   getLatestProducts().then(setProducts);
   // };
 
-  getLatestProducts().then(setProducts);
+  // getLatestProducts().then(setProducts);
   // console.warn(products);
 
   useEffect(() => {
     getLatestProducts().then(setProducts);
     // theLatestProducts();
-  }, [products]);
+  }, []);
 
   return (
     <div>
-      {/* <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button> */}
       <Table>
         <thead>
           <tr>
-            <th colSpan={8}>#</th>
+            <th>Feature Products</th>
+            <th>
+              <Link passHref href="/products">
+                <Button variant="success">See All Products</Button>
+              </Link>
+            </th>
             {/* <th colSpan={8}>Last Name</th>
             <th>Username</th> */}
           </tr>
@@ -40,33 +39,16 @@ function Home() {
         <tbody>
           {
             products.map((p) => (
-              <tr>
+              <tr key={p.id}>
                 <td>{p.name}</td>
-                {/* <td colSpan={8}>Mark</td>
-                <td>@mdo</td> */}
                 <td>
-                  <Link passHref href={`/products/${p.Id}`}>
-                    <Button variant="link">Link</Button>
+                  <Link passHref href={`/products/${p.id}`}>
+                    <Button variant="link">https://{p.name}/details</Button>
                   </Link>
                 </td>
               </tr>
             ))
           }
-          {/* <tr>
-            <td>1</td>
-            <td colSpan={8}>Mark</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td colSpan={8}>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={8}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr> */}
         </tbody>
       </Table>
     </div>
