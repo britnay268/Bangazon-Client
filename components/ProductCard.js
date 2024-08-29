09cbbbac38c1bc265fbaa83ec70e07cda906dc44
Britnay Gore <britnaygore268@gmail.com>
2024-08-28 21:38:49 -0400

    Can now see all products when the button is clicked on the homepage

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,45 +1,40 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import { Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+import Link from 'next/link';
 
-export default function ProductCard() {
+export default function ProductCard({ productObj }) {
   return (
     <div>
       <Card style={{ width: '55rem', display: 'grid', gridTemplateColumns: '200px auto' }}>
-        <Card.Img variant="top" src="https://image.shutterstock.com/image-vector/dotted-spiral-vortex-royaltyfree-images-600nw-2227567913.jpg" />
+        <Card.Img variant="top" src={productObj.imageUrl} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up
-            the
-            bulk of the cards content.
-          </Card.Text>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-
+          <Card.Title>{productObj.name}</Card.Title>
+          <Card.Text>{productObj.user.username}</Card.Text>
+          <Card.Text>{productObj.description}</Card.Text>
+          <Card.Text>Price: ${productObj.price}</Card.Text>
+          <Card.Text>{productObj.quantity > 0 ? 'In Stock' : 'Out of Stock'}</Card.Text>
+          <Link passHref href={`/products/${productObj.id}`}>
+            <Button variant="link">Details</Button>
+          </Link>
         </Card.Body>
       </Card>
     </div>
   );
 }
 
-// ProductCard.propTypes = {
-//   productObj: PropTypes.shape({
-//     id: PropTypes.number,
-//     name: PropTypes.string,
-//     price: PropTypes.number,
-//     description: PropTypes.string,
-//     quantity: PropTypes.number,
-//     imageUrl: PropTypes.string,
-//     user: PropTypes.shape({
-//       name: PropTypes.string,
-//       username: PropTypes.string,
-//       seller: PropTypes.bool,
-//     }),
-//     categories: PropTypes.shape({
-//       name: PropTypes.string,
-//     }),
-//   }).isRequired,
-// };
+ProductCard.propTypes = {
+  productObj: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    price: PropTypes.number,
+    description: PropTypes.string,
+    quantity: PropTypes.number,
+    imageUrl: PropTypes.string,
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      username: PropTypes.string,
+      seller: PropTypes.bool,
+    }),
+  }).isRequired,
+};
