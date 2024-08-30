e7f3f5fd3ab21756534fc94fccc82a8ea588de8e
Britnay Gore <britnaygore268@gmail.com>
2024-08-30 00:26:22 -0400

    A product can be added to the cart from the details page

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -4,12 +4,18 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 import { getProductById } from '../../api/ProductData';
+import { addProductToOrder } from '../../api/OrderData';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function ProductDetails() {
   const [productDetails, setProductDetails] = useState([]);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
 
   const router = useRouter();
   const { productId } = router.query;
+
+  const { user } = useAuth();
+  const userId = user.user.id;
 
   useEffect(() => {
     getProductById(productId).then(setProductDetails);
@@ -29,7 +35,8 @@ export default function ProductDetails() {
       <p>{productDetails.description}</p>
       <p>{productDetails.quantity > 0 ? 'In Stock' : 'Out of Stock'}</p>
       <p>Price Per Unit: {productDetails.price}</p>
-      <Button>Add to Cart</Button>
+      <Button onClick={() => addProductToOrder(productId, userId).then(() => { setIsAddedToCart(true); })}>Add to Cart</Button>
+      {isAddedToCart && <span>Product added to cart.</span>}
     </>
   );
 }
