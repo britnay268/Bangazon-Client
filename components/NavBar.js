18cb6945ded933af46eace164d321e3a2688331a
Britnay Gore <britnaygore268@gmail.com>
2024-08-29 22:04:37 -0400

    Added cart to navbar that goes to the cart page

--- components/NavBar.js
+++ components/NavBar.js
@@ -18,18 +18,23 @@ export default function NavBar() {
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto dflex">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
             </Link>
-            <Link passHref href="/delete-me">
-              <Nav.Link>Delete Me</Nav.Link>
+            <Link passHref href="/products">
+              <Nav.Link>Products</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>
               Sign Out
             </Button>
           </Nav>
+          <div className="d-flex">
+            <Link passHref href="/cart">
+              <Nav.Link style={{ color: 'white' }}>Cart</Nav.Link>
+            </Link>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
