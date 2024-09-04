947ed584b34526aac090b937dcef7a7bc0605fdd
Britnay Gore <britnaygore268@gmail.com>
2024-09-04 17:46:32 -0400

    Made sure on sign in the home page shows

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Button, Image } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 
 export default function Profile() {
+  const router = useRouter();
   const { user } = useAuth();
+
+  const handleChange = async () => {
+    await router.push('/');
+    signOut();
+  };
 
   return (
     <div className="profile">
@@ -13,7 +20,7 @@ export default function Profile() {
       <p>{user.fbUser.displayName}</p>
       <p>{user.fbUser.email}</p>
       <p>Seller: {user.user.seller ? 'Yes' : 'No'}</p>
-      <Button variant="danger" onClick={signOut}>
+      <Button variant="danger" onClick={handleChange}>
         Sign Out
       </Button>
     </div>
