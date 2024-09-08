6c19dca58319e4e71eeb386b30f406d70acd897e
Britnay Gore <britnaygore268@gmail.com>
2024-09-07 22:02:13 -0400

    User can now see order history which shows all completed orders

---
+++ pages/orderhistory.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { getCompletedOrders } from '../api/UserData';
+import { useAuth } from '../utils/context/authContext';
+import OrderCards from '../components/OrderCards';
+
+export default function OrderHistory() {
+  const [orders, setOrders] = useState([]);
+
+  const { user } = useAuth();
+
+  const userId = user.user.id;
+
+  const completedOrders = () => {
+    getCompletedOrders(userId).then(setOrders);
+  };
+
+  console.warn(orders);
+
+  useEffect(() => {
+    completedOrders();
+  }, []);
+
+  return (
+    <div>
+      <h2 style={{ marginTop: '25px' }}>Completed Orders</h2>
+      <div>
+        {orders.length === 0
+          ? <h1>You have no Completed Orders</h1>
+          : orders.map((order) => (
+            <OrderCards key={order.id} orderObj={order} />
+          ))}
+      </div>
+    </div>
+  );
+}
