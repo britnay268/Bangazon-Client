ef1f8facd9e73976592c3c6d89be56d680321ed6
Britnay Gore <britnaygore268@gmail.com>
2024-09-10 19:38:32 -0400

    Modified order cards to be a dropdown to show and hide order details

--- components/OrderCards.js
+++ components/OrderCards.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import {
-  Button, Card, ListGroup, Nav,
+  Button, Card, Dropdown, ListGroup, Nav,
 } from 'react-bootstrap';
 import Link from 'next/link';
 import { BiDetail } from 'react-icons/bi';
@@ -14,38 +14,53 @@ import { BiDetail } from 'react-icons/bi';
 export default function OrderCards({ orderObj }) {
   return (
     <>
-      <ListGroup style={{ marginBottom: '20px' }}>
-        <ListGroup.Item>
-          <div><strong>Order Number:</strong> {orderObj.orderNum}</div>
-          <div><strong>Order Date:</strong> {orderObj.datePlaced.split('T')[0]}</div>
-        </ListGroup.Item>
+      <Dropdown style={{ marginBottom: '20px' }}>
+        <>
+          <ListGroup>
 
-        {orderObj.products.map((product) => (
-          <ListGroup.Item key={product.id}>
-            <Card className="checkout-card">
-              <Card.Img variant="top" src={product.imageUrl} />
-              <Card.Body>
-                <div className="productDetails">
-                  <div>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Link passHref href={`/sellerspage/${product.user.id}`}>
-                      <Nav.Link variant="link" style={{ color: 'black', marginBottom: '50px' }}>{product?.user?.username}</Nav.Link>
-                    </Link>
-                  </div>
-                  <div>
-                    <Card.Text className="price">Price: ${product.price}</Card.Text>
-                    <Card.Text className={product.quantity > 0 ? 'inStock' : 'outOfStock'}>{product.quantity > 0 ? 'In Stock' : 'Out of Stock'}</Card.Text>
-                  </div>
-                </div>
-                <Link passHref href={`/products/${orderObj.id}`}>
-                  <Button variant="link" className="element detailsBtn"><BiDetail /></Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </ListGroup.Item>
-        ))}
+            <ListGroup.Item>
+              <div><strong>Order Number:</strong> {orderObj.orderNum}</div>
+              <div><strong>Order Date:</strong> {orderObj.datePlaced.split('T')[0]}</div>
+            </ListGroup.Item>
 
-      </ListGroup>
+            <Dropdown.Toggle
+              style={{
+                backgroundColor: 'white', color: 'black', border: 'none', widdth: '100px',
+              }}
+              id="dropdown-basic"
+            >
+              View Order Details
+            </Dropdown.Toggle>
+          </ListGroup>
+        </>
+
+        <Dropdown.Menu style={{ width: '69.8rem' }}>
+          {orderObj.products.map((product) => (
+            <Dropdown.Item key={product.id}>
+              <Card className="checkout-card">
+                <Card.Img variant="top" src={product.imageUrl} />
+                <Card.Body>
+                  <div className="productDetails">
+                    <div>
+                      <Card.Title>{product.name}</Card.Title>
+                      <Link passHref href={`/sellerspage/${product.user.id}`}>
+                        <Nav.Link variant="link" style={{ color: 'black', marginBottom: '50px' }}>{product?.user?.username}</Nav.Link>
+                      </Link>
+                    </div>
+                    <div>
+                      <Card.Text className="price">Price: ${product.price}</Card.Text>
+                      <Card.Text className={product.quantity > 0 ? 'inStock' : 'outOfStock'}>{product.quantity > 0 ? 'In Stock' : 'Out of Stock'}</Card.Text>
+                    </div>
+                  </div>
+                  <Link passHref href={`/products/${orderObj.id}`}>
+                    <Button variant="link" className="element detailsBtn"><BiDetail /></Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
     </>
   );
 }
