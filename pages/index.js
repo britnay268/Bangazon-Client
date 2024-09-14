6801ab0b5e250cac943b50151972ba2bb120dac5
Britnay Gore <britnaygore268@gmail.com>
2024-09-14 17:09:44 -0400

    Added a part of the home page to show the category count and the first 3 products in the catgory

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
 import Link from 'next/link';
-import { getLatestProducts, getProducts } from '../api/ProductData';
+import { getLatestProducts } from '../api/ProductData';
 import CategoriesButton from '../components/CategoriesButton';
 import { getCategoryById } from '../api/CategoryData';
 import ProductCard from '../components/ProductCard';
@@ -10,22 +10,15 @@ import { addProductToOrder } from '../api/OrderData';
 
 function Home() {
   const [products, setProducts] = useState([]);
-  const [categoryId, setCategoryId] = useState(null);
+
   // This is implemented to show the table based only for the latest products and not for products by category
   const [showTable, setShowTable] = useState(true);
+  // This is to store the categoryname from
+  const [categoryName, setCategoryName] = useState(null);
 
   useEffect(() => {
     getLatestProducts().then(setProducts);
   }, []);
-
-  const getAllTheProducts = () => {
-    getProducts().then(setProducts);
-    setCategoryId(null);
-    // If the show table is true, set the show table to false to show all the products in the product card when a user clicks the All category button as the getAllTheProducts function is called in the ProductCard
-    if (showTable) {
-      setShowTable(false);
-    }
-  };
 
   const addProductToCart = async (productId, userId) => {
     addProductToOrder(productId, userId);
@@ -34,17 +27,17 @@ function Home() {
     setProducts(products.map((product) => (product.id === productId ? { ...product, quantity: product.quantity - 1 } : product)));
   };
 
-  const productsbyCategorgory = async (categoryID) => {
-    // getCategoryById(categoryId).then(setProducts);
+  const productsbyCategory = async (categoryID, catName) => {
     const categoryData = await getCategoryById(categoryID);
     setProducts(categoryData.products);
-    setCategoryId(categoryId);
+
+    setCategoryName(catName);
     setShowTable(false);
   };
 
   return (
     <div style={{ margin: '10px 80px' }}>
-      <CategoriesButton filterBy={productsbyCategorgory} filterAll={getAllTheProducts} />
+      <CategoriesButton filterBy={productsbyCategory} />
       {showTable
         ? (
           <Table style={{ marginTop: '40px' }}>
@@ -76,6 +69,15 @@ function Home() {
         )
         : (
           <div>
+            <h6 style={{ marginTop: '20px', marginBottom: '0px' }}>{categoryName} ({products.length})</h6>
+            <div className="categoryLinks">
+              {/* Added slice to show only 3 products */}
+              {products.slice(0, 3).map((product) => (
+                <Link passHref href={`/products/${product.id}`}>
+                  <Button variant="link">{product.name}</Button>
+                </Link>
+              ))}
+            </div>
             <div className="product-page">
               {products.length === 0 ? <h1>You have no Products</h1> : products.map((product) => (
                 // The addProduct is using the api to
